1d2ac9f60a6987c4ed4cfc2396fd799df08a39c2
Hans Hoeglund <hans@hanshoglund.se>
2020-04-16 13:13:30 +0100

    CI config for Nix/Cabal

---
+++ default.nix
@@ -0,0 +1,30 @@
+
+let
+  opts = {
+    packageOverrides = pkgs: rec {
+      haskellPackages = pkgs.haskellPackages.override {
+        overrides = haskellPackagesNew: haskellPackagesOld: rec {
+          # No overrides
+        };
+      };
+    };
+  };
+
+  pkgs = import (builtins.fetchTarball {
+    url = https://github.com/nixos/nixpkgs/archive/f74f2f354866c828248a419ef9a2cbddc793b7f9.tar.gz;
+    # Hash obtained using `nix-prefetch-url --unpack <url>`
+    sha256 = "1jxb2kb83mrmzg06l7c1zw9pikk2l1lpg8dl0rvni65bgmlxf7xy";
+  }) { config = opts; };
+in
+
+pkgs.stdenv.mkDerivation {
+  name = "iso-deriving";
+  buildInputs = [
+    (
+    pkgs.haskellPackages.ghcWithPackages (pkgs:
+        [ pkgs.cabal-install
+        ])
+    )
+  ];
+}
+
