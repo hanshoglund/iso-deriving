ca38a7843c8bcf71c40d6462230bdbcf3976dcb7
Hans Hoeglund <hans@hanshoglund.se>
2020-04-23 11:50:11 +0100

    Cleanup Nix expression

--- default.nix
+++ default.nix
@@ -12,7 +12,6 @@ let
 
   pkgs = import (builtins.fetchTarball {
     url = https://github.com/nixos/nixpkgs/archive/56f7c93a2fcd325506a6bfa7bf8e9faa2a2c7530.tar.gz;
-    # Hash obtained using `nix-prefetch-url --unpack <url>`
     sha256 = "0fm9a24kjailx9qlyvwwa3a635m8pdk00329rldc541zhpmrklib";
   }) { config = opts; };
 in
@@ -22,8 +21,8 @@ pkgs.stdenv.mkDerivation {
   buildInputs = [
     (
     pkgs.haskellPackages.ghcWithPackages (pkgs:
-        [ pkgs.cabal-install
-        ])
+      [ pkgs.cabal-install
+      ])
     )
   ];
 }
